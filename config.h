9f68e47f2e41e1fd260659f8e99ab7d9d5f3e33a
sconnet <empty>
2001-04-21 06:19:51 +0000

    developmental changes

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 //
 // Source File Name : config.h
 //
-// Version          : $Id: $
+// Version          : $Id: config.h,v 1.1 2001/04/21 02:50:32 sconnet Exp sconnet $
 //
 // File Overview    : Reads in the configuration file once and puts
 //                    each name value pair into a map for quick
@@ -15,7 +15,10 @@
 //
 // Revision History : 
 //
-// $Log: $
+// $Log: config.h,v $
+// Revision 1.1  2001/04/21 02:50:32  sconnet
+// Initial revision
+//
 //
 //*****************************************************************************
 
@@ -24,34 +27,30 @@
 
 #include <map>
 #include <string>
-#include <pthread.h>
 
 typedef map<string, string> CMap;
 
 class CConfig
 {
-public:
-    CConfig();
-    CConfig(const char* szFilename);
-    virtual ~CConfig();
-    
-    bool Read(const char* szFilename);
+ public:
+  CConfig() {}
+  virtual ~CConfig() {}
+  
+  virtual bool Read(const string& sFilename);  
+  virtual void Dump();
+  
+  inline string& operator()(const char* szName, const char* szDefault = NULL)
+    { return GetValueAsStr(szName, szDefault); }
+  
+  inline int operator()(const char* szName, int nDefault = 0)
+    { return GetValueAsInt(szName, nDefault); }
 
-    inline char* operator()(char* szName, char* szDefault)
-        { return GetValueAsString(szName, szDefault); }
-
-    inline int operator()(char* szName, int nDefault)
-        { return GetValueAsInt(szName, nDefault); }
-
-    void Dump();
-
-private:
-    void ParseString(char* s);
-    int GetValueAsInt(char* szName, int nDefault);
-    char* GetValueAsString(char* szName, char* szDefault);
-
-    CMap m_map;
-    pthread_mutex_t m_lock;
+ protected:
+  string& GetValueAsStr(const char* szName, const char* szDefault = NULL);
+  int GetValueAsInt(const char* szName, int nDefault = 0);
+  
+ private:
+  CMap m_map;
 };
 
 #endif // __CONFIG_H_
